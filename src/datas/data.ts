eb7d649ac1edcae05c97828c361f439a0f70d042
Hoai-Minh-tanaakk <phan.van.hoai.minh@tanaakk.com>
2024-03-23 21:59:58 +0700

    setup source

---
+++ src/datas/data.ts
@@ -0,0 +1,91 @@
+interface valueType {
+    product: string;
+    transactions: number;
+    dollarValue: number;
+    quantity: number;
+    containers: number;
+    weight: number;
+}
+
+const data: valueType[] = [
+    {
+        product: "Molasses",
+        transactions: 1886,
+        dollarValue: 5443000,
+        quantity: 175132,
+        containers: 498,
+        weight: 2951359,
+    },
+    {
+        product: "Tobacco",
+        transactions: 2106,
+        dollarValue: 2104177,
+        quantity: 1804932,
+        containers: 214,
+        weight: 3012854,
+    },
+    {
+        product: "AI Fakher",
+        transactions: 2537,
+        dollarValue: 4124177,
+        quantity: 19943685,
+        containers: 256,
+        weight: 2629463,
+    },
+    {
+        product: "Carton",
+        transactions: 2991,
+        dollarValue: 1304177,
+        quantity: 32832850,
+        containers: 451,
+        weight: 4010177,
+    },
+    {
+        product: "Shoe",
+        transactions: 3093,
+        dollarValue: 4377,
+        quantity: 15997562,
+        containers: 313,
+        weight: 3027086,
+    },
+    {
+        product: "Banana",
+        transactions: 3132,
+        dollarValue: 627,
+        quantity: 32484002,
+        containers: 213,
+        weight: 3803838,
+    },
+    {
+        product: "Cotton",
+        transactions: 3259,
+        dollarValue: 4104177,
+        quantity: 35729599,
+        containers: 213,
+        weight: 3803838,
+    },
+    {
+        product: "Banana",
+        transactions: 3132,
+        dollarValue: 627,
+        quantity: 32484002,
+        containers: 213,
+        weight: 3803838,
+    },
+    {
+        product: "Cotton",
+        transactions: 3259,
+        dollarValue: 4104177,
+        quantity: 35729599,
+        containers: 112,
+        weight: 4504487,
+    },
+    {
+        product: "Shirt Button",
+        transactions: 3279,
+        dollarValue: 3404277,
+        quantity: 30789411,
+        containers: 662,
+        weight: 7186776,
+    }
+]
